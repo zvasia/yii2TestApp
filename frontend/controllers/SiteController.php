cdf577a9b58c74a78c47080db79e0c6645d16e83
vasiliy <zvasia@gmail.com>
2020-05-25 05:31:08 +0300

    init

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -75,7 +75,7 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        return $this->redirect(['users/index']);
     }
 
     public function actionUserList()
